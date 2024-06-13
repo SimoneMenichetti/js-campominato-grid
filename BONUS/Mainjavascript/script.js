760eeacff5d17ba4db0a9287b709fdef54f0317b
simone menichetti <simon3menichettui95@gmail.com>
2024-06-13 17:13:49 +0200

    feat:(js script add const for call interact utent select difficulty)

--- BONUS/Mainjavascript/script.js
+++ BONUS/Mainjavascript/script.js
@@ -6,6 +6,11 @@ const playButton = document.querySelector('.playButton');
 // Variabile di riferimento per richiamare il container della griglia
 const gridContainer = document.getElementById('grid-container');
 
+                // BONUS
+// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+// variabile di riferimento per richiamare la select della difficoltà
+const difficultySelect = document.getElementById('difficulty');
+
 
 // funzione per lo start del gioco al momento dell ascolto evento
 
